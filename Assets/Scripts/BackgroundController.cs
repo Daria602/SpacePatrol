eb7c645f29cbf614ca9b538010f5f2f7d9b6e8fe
Daria602 <izotovadaria0602@gmail.com>
2022-11-05 14:34:20 +0200

    Change the speed of background moving

--- Assets/Scripts/BackgroundController.cs
+++ Assets/Scripts/BackgroundController.cs
@@ -6,13 +6,15 @@ public class BackgroundController : MonoBehaviour
 {
     public float yStart;
     public float yDifference;
-  
+    public float toSubstract; // used to move background 
+
 
     // Start is called before the first frame update
     void Start()
     {
         yStart = transform.localPosition.y;
         yDifference = 222;
+        toSubstract = 0.2f;
     }
 
     // Update is called once per frame
@@ -24,7 +26,7 @@ public class BackgroundController : MonoBehaviour
     private void VerticalMovement()
     {
         float yCurrent = transform.localPosition.y;
-        float toSubstract = 0.3f;
+        
         if (yCurrent - toSubstract > yStart - yDifference)
         {
             transform.localPosition = new Vector2(transform.localPosition.x, yCurrent - toSubstract);
