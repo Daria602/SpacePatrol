3712cafe5e294d04c103a1bad77523c311a98867
Andrei Gatej <andreigtj01@gmail.com>
2022-11-11 19:57:34 +0200

    feat: make `Continue` text fade in after vertical movement is done

--- Assets/Scripts/BackgroundController.cs
+++ Assets/Scripts/BackgroundController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class BackgroundController : MonoBehaviour
 {
@@ -8,6 +9,9 @@ public class BackgroundController : MonoBehaviour
     public float yDifference;
     public float toSubstract; // used to move background 
 
+    [SerializeField] private TextMeshProUGUI continueText;
+
+    private bool hasContinueTextTransitionStarted = false;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +19,8 @@ public class BackgroundController : MonoBehaviour
         yStart = transform.localPosition.y;
         yDifference = 222;
         toSubstract = 0.2f;
+
+        hideContinueText();
     }
 
     // Update is called once per frame
@@ -30,6 +36,30 @@ public class BackgroundController : MonoBehaviour
         if (yCurrent - toSubstract > yStart - yDifference)
         {
             transform.localPosition = new Vector2(transform.localPosition.x, yCurrent - toSubstract);
+        } else if (!hasContinueTextTransitionStarted) {
+            hasContinueTextTransitionStarted = true;
+            showContinueText();
         }
+    }
+
+    private IEnumerator FadeIn () {
+        float duration = 2f;
+        float currentTime = 0f;
+        while (currentTime < duration)
+        {
+            float alpha = Mathf.Lerp(0f, 1f, currentTime / duration);
+            continueText.color = new Color(continueText.color.r, continueText.color.g, continueText.color.b, alpha);
+            currentTime += Time.deltaTime;
+            yield return null;
+        }
+        yield break;
+    }
+
+    private void hideContinueText () {
+        continueText.color = new Color(continueText.color.r, continueText.color.g, continueText.color.b, 0f);
+    }
+
+    private void showContinueText () {
+        StartCoroutine(FadeIn());
     }
 }
