3fe2d192f81aaaf2ca023d5dc2adae7d0191e7d6
AndreeaGeorgescu7 <andramaria.georgescu@gmail.com>
2022-11-14 15:02:52 +0200

    added readme

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,68 +1,141 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+public enum PlayerNumber
+{
+    PlayerOne = 0,
+    PlayerTwo = 1,
+}
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody2D body;
+    public PlayerNumber PlayerNr;
+    private Rigidbody2D _rigidbody;
     [SerializeField] private float speed;
-    [HideInInspector] public bool isGoingLeft;
-    private Vector2 goingLeft, goingRight, isUp, isDown, currentPosition;
-    private bool grounded;
+    [SerializeField] private float groundDistance;
+
+    //The layers that are considered to be ground
+    [SerializeField] private LayerMask groudMask;
+    private SpriteRenderer _spriteRenderer;
+
+    //[HideInInspector] public bool isGoingLeft;
+    // private Vector2 goingLeft, goingRight, isUp, isDown, currentPosition;
+
+
+    //function for changing the gravity
+    public bool IsUpsideDown
+    {
+        get => _isUpsideDown;
+        set
+        {
+            if (value == _isUpsideDown) return;
+            _isUpsideDown = value;
+            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y*-1, transform.localScale.z);
+            //Change the gravity scaling 
+            _rigidbody.gravityScale *= -1;
+        }
+    }
+
+    private bool _isUpsideDown = false;
+    //function for changing the x axis orientation
+    public bool FacingRight
+    {
+        get => _facingRight;
+        set
+        {
+            if (value == _facingRight) return;
+            _facingRight = value;
+            transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+        }
+    }
+
+    private bool _facingRight;
+    private bool _isGrounded;
+    
+    RaycastHit2D hit;
 
     private void Awake() // grab references
     {
-        body = GetComponent<Rigidbody2D>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _facingRight = Mathf.Sign(transform.localScale.x) > 0 ? true : false;
+        _isUpsideDown= Mathf.Sign(transform.localScale.y) < 0 ? true : false;
+        _spriteRenderer = GetComponentInChildren<SpriteRenderer>();
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        goingLeft = new Vector2(-transform.localScale.x, transform.localScale.y);
-        goingRight = new Vector2(transform.localScale.x, transform.localScale.y);
+        //goingLeft = new Vector2(-transform.localScale.x, transform.localScale.y);
+        //goingRight = new Vector2(transform.localScale.x, transform.localScale.y);
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
+        float horizontalInput = 0f;
+        bool shouldJump = false;
+        switch (PlayerNr)
+        {
+            case PlayerNumber.PlayerOne:
+                horizontalInput = Input.GetAxis("Horizontal_One");
+                shouldJump = Input.GetButtonDown("Jump_One");
+                break;
+            case PlayerNumber.PlayerTwo:
+                horizontalInput = Input.GetAxis("Horizontal_Two");
+                shouldJump = Input.GetButtonDown("Jump_Two");
+                break;
+        }
 
         // direction going
-        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+        _rigidbody.velocity = new Vector2(horizontalInput * speed, _rigidbody.velocity.y);
 
-        // direction facing
-        if (horizontalInput > 0.01f)
-        {
-            transform.localScale = goingRight;
-        }
-        else if (horizontalInput < -0.0f)
-        {
-            transform.localScale = goingLeft;
-        }
+        FacingRight = horizontalInput >= 0 ? true : false;
 
-        if (Input.GetKey(KeyCode.UpArrow) && grounded)
-            Jump();
+        hit = Physics2D.Raycast(transform.position, Vector2.down*(IsUpsideDown ? -1 : 1), groundDistance, groudMask);
+        _isGrounded = hit.collider != null;
+
+       if (shouldJump && _isGrounded)
+        Jump();
+
+       //For debug 
+       ///if u press tab it will change the gravity
+       if(Input.GetKeyDown(KeyCode.Tab))
+        {
+            IsUpsideDown = !IsUpsideDown;
+        }
     }
 
     private void Jump()
     {
-        body.velocity = new Vector2(body.velocity.x, speed);
-        grounded = false;
+        _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, speed * (IsUpsideDown ? -1 : 1));
     }
+
+    //This type collision detection is bad because if we hit our head with a platform we will be grounded
+    //Use raycast instead
+    /*
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "platform")
-            grounded = true;
+            _grounded = true;
     }
+    */
 
     private void OnMouseDown()
     {
-        GetComponent<SpriteRenderer>().color = Color.red;
+        if(_spriteRenderer)
+        _spriteRenderer.color = Color.red;
     }
 
     private void OnMouseUp()
     {
-        GetComponent<SpriteRenderer>().color = Color.white;
+        if (_spriteRenderer)
+            _spriteRenderer.color = Color.white;
+    }
+    private void OnDrawGizmosSelected()
+    {
+        //for debug
+        Gizmos.DrawLine(transform.position, new Vector3(transform.position.x, transform.position.y - groundDistance, transform.position.z));
     }
 }
